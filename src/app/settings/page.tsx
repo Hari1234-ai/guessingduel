f4e2ca7be8707a71312e961bd042632cbf0898fc
chenna hari krishna <aravindhari@MacBookAir.lan>
2026-03-22 13:25:30 +0530

    style: remove coins and profile card from settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -55,10 +55,6 @@ export default function SettingsPage() {
       <div className="absolute top-1/4 -left-24 w-96 h-96 bg-blue-600/5 rounded-full blur-[120px]" />
       <div className="absolute bottom-1/4 -right-24 w-96 h-96 bg-purple-600/5 rounded-full blur-[120px]" />
 
-      {/* Global Avatar */}
-      <div className="fixed top-6 right-6 z-50">
-        <AvatarDropdown />
-      </div>
 
       <div className="max-w-4xl mx-auto relative z-10 pt-12">
         <header className="flex items-center justify-between mb-12">
